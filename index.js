f57cc84efcd0c577e47de0fce7aa321d2d72cfcd
amandipd <amandip@berkeley.edu>
2025-12-02 19:41:49 -0800

    add middlware

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!!</h1>");
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// we use app.use() when we want to use middleware
 
 const PORT = process.env.PORT || 5000;
 
