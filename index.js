e0756c1e65acbfc1c7e4c813c4176cf145cbe09c
amandipd <amandip@berkeley.edu>
2025-12-21 18:49:31 -0800

    add members.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const path = require("path");
+const moment = require("moment");
+const logger = require("./middleware/logger");
+
 const app = express();
 
+// Init middleware
+// app.use(logger);
+
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 // we use app.use() when we want to use middleware
 
